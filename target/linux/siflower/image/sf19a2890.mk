69a3f0a75952dab5887be2bc8966ded2e5aaf8e2
Daniel Golle <daniel@makrotopia.org>
2026-01-21 11:58:18 +0000

    siflower: sf19a2890: add support for GL.iNet GL-SFT1200
    Add basic support for GL.iNet GL-SFT1200 router
    
    Hardware:
     * Siflower SF19A2890 SoC
      - dual-core MIPS interAptiv @1GHz
      - Synopsys DesignWare DWMAC1000 Ethernet
      - Synopsys DesignWare DWC USB 2.0 controller
      - Catena Wi-Fi 11ac dual-band 2T2R (unsupported)
      - hardware flow-offloading engine (unsupported, probably incompatible
        with MaxLinear's 8-byte DSA tag at hardware level)
     * 128 MB DDR3 RAM
     * 128 MB GigaDevice SPI-NAND flash
     * Intel/MaxLinear GSW141 switch IC
      - 3x 1Gbit/s Ethernet RJ-45 ports
     * might contain ET6326 LED controller? (unsupported)
     * USB 2.0 host port
     * internal unpopulater 3.3V TTL level UART (115200, 8n1)
     * Power: 5V 3A via USB-C connector
     * Reset button
     * Sliding switch (intended for switching between AP and repeater mode)
    
    Installation:
    Use *-factory.img file in U-Boot Web flash tool or *-sysupgrade.tar
    with vendor stock firmware.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/siflower/image/sf19a2890.mk
+++ target/linux/siflower/image/sf19a2890.mk
@@ -20,3 +20,18 @@ define Device/siflower_sf19a2890-evb
   DEVICE_PACKAGES := kmod-switch-rtl8367b swconfig
 endef
 TARGET_DEVICES += siflower_sf19a2890-evb
+
+define Device/glinet_gl-sft1200
+  DEVICE_VENDOR := GL.iNet
+  DEVICE_MODEL :=  GL-SFT1200
+  DEVICE_DTS := sf19a2890_glinet_gl-sft1200
+  DEVICE_PACKAGES := kmod-dsa-mxl-gsw1xx kmod-phy-sf19a2890-usb kmod-usb-dwc2
+  IMAGE_SIZE := 122880k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE := 3072k
+  IMAGES := factory.img sysupgrade.tar
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
+  IMAGE/factory.img := append-kernel | pad-to 4096k | append-ubi
+endef
+TARGET_DEVICES += glinet_gl-sft1200
--- target/linux/siflower/sf19a2890/base-files/lib/upgrade/platform.sh
+++ target/linux/siflower/sf19a2890/base-files/lib/upgrade/platform.sh
@@ -5,6 +5,10 @@ platform_check_image() {
 	local board=$(board_name)
 
 	case "$board" in
+	glinet,gl-sft1200)
+		nand_do_platform_check "$board" "$1"
+		return $?
+		;;
 	*)
 		return 0
 		;;
@@ -15,6 +19,9 @@ platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
+	glinet,gl-sft1200)
+		nand_do_upgrade "$1"
+		;;
 	*)
 		default_do_upgrade "$1"
 		;;
